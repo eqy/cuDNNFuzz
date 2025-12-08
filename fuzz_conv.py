fd2a3ee90fd7ca576e08d9740809902ba3581877
Eddie Yan <eddiey@nvidia.com>
2025-12-08 23:48:34 +0000

    forward conv

--- fuzz_conv.py
+++ fuzz_conv.py
@@ -1,6 +1,6 @@
 import math
 import os
-
+import re
 import torch
 
 #os.environ['CUDNN_LOGLEVEL_DBG'] = '1'
@@ -9,18 +9,20 @@ import torch
 #os.environ['CUDNN_FRONTEND_LOG_FILE'] = f'sdpa_frontend_rank_{int(os.environ['LOCAL_RANK'])}.log'
 #os.environ['TORCH_CUDNN_V8_API_LRU_CACHE_LIMIT'] = '-1'
 
-MAX_SPATIAL = 32768
-MAX_BATCH_SIZE = 512
-MAX_ELEM = 2**32
-MAX_KERNEL = 13
+MAX_SPATIAL = 512
+MAX_BATCH_SIZE = 128
+MAX_ELEM = 2**31
+MAX_KERNEL = 7
 MAX_STRIDE = 3
-MAX_DILATION = 3
-MAX_CHANNEL = 2048
+MAX_DILATION = 1
+MAX_CHANNEL = 1024
 DTYPES = [torch.float32, torch.bfloat16, torch.half]
 SPATIAL_DIMS = [2, 3]
 CHECK_REF = True
 
+
 while True:
+    print("print")
     dtype = DTYPES[torch.randint(low=0, high=len(DTYPES), size=(1,)).item()]
     num_spatial_dim = SPATIAL_DIMS[torch.randint(low=0, high=len(SPATIAL_DIMS), size=(1,)).item()]
     spatial_sizes = list()
@@ -41,6 +43,7 @@ while True:
         out_channel = in_channel
     else:
         out_channel = torch.randint(1, high=min(MAX_CHANNEL, high_channel), size=(1,)).item()
+    groups = 1 if not depthwise else in_channel
     high_batch_size = MAX_ELEM//(max(in_channel, out_channel)*(math.prod(spatial_sizes)))
     if high_batch_size <= 1:
         continue
@@ -48,11 +51,11 @@ while True:
 
     kernel_sizes = list()
     for _ in range(num_spatial_dim):
-        kernel_size = torch.randint(1, high=MAX_KERNEL+1, size=(1,)).item()
+        kernel_size = torch.randint(1, high=min(MAX_KERNEL, min(spatial_sizes))+1, size=(1,)).item()
         kernel_sizes.append(kernel_size)
 
     input_shape = [batch_size, in_channel] + spatial_sizes
-    weight_shape = [out_channel, in_channel] + kernel_sizes
+    weight_shape = [out_channel, in_channel // groups] + kernel_sizes
     memory_formats = [torch.channels_last, torch.contiguous_format] if num_spatial_dim == 2 else [torch.channels_last_3d, torch.contiguous_format]
     memory_format = memory_formats[torch.randint(low=0, high=len(memory_formats), size=(1,)).item()]
 
@@ -63,7 +66,6 @@ while True:
 
     stride = torch.randint(low=1, high=MAX_STRIDE+1, size=(1,)).item()
     dilation = torch.randint(low=1, high=MAX_DILATION+1, size=(1,)).item()
-    groups = 1 if not depthwise else in_channels
 
     if num_spatial_dim == 2:
         out = torch.nn.functional.conv2d(inp, weight, stride=stride, padding=0, dilation=dilation, groups=groups)
@@ -74,12 +76,18 @@ while True:
         inp_ref = inp.cpu().detach().clone()
         weight_ref = weight.cpu().detach().clone()
         if num_spatial_dim == 2:
-            out = torch.nn.functional.conv2d(inp_ref, weight_ref, stride=stride, padding=0, dilation=dilation, groups=groups)
+            out_ref = torch.nn.functional.conv2d(inp_ref, weight_ref, stride=stride, padding=0, dilation=dilation, groups=groups)
         else:
-            out = torch.nn.functional.conv3d(inp_ref, weight_ref, stride=stride, padding=0, dilation=dilation, groups=groups)
+            out_ref = torch.nn.functional.conv3d(inp_ref, weight_ref, stride=stride, padding=0, dilation=dilation, groups=groups)
 
-    torch.testing.assert_close(out_ref, out, atol=5e-3, rtol=1e-3)
+        try:
+            torch.testing.assert_close(out_ref.to(out.device), out, atol=1., rtol=5e-2)
+        except AssertionError as e:
+            match = re.search(r'Mismatched elements:.*?\((\d+\.?\d*)%\)', str(e), re.MULTILINE)
+            if float(match.group(1)) < 5.0:
+                print("mismatches, but less than 5%, skipping...")
+            else:
+                raise e
 
     out.backward(torch.randn_like(out))
-
-    print(dtype, batch_size, spatial_sizes, in_channel, out_channel, depthwise)
+    print(f"dtype {dtype} inp shape {inp.shape} weight shape {weight.shape}, groups {groups}, stride {stride}")
